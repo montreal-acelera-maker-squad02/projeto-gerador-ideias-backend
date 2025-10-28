58b6af2ccaefaa09fcf013e5be7221a6cc103871
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-10-28 18:55:14 -0300

    feat: DTOs Ollama

---
+++ src/main/java/projeto_gerador_ideias_backend/dto/OllamaResponse.java
@@ -0,0 +1,18 @@
+package projeto_gerador_ideias_backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OllamaResponse {
+
+    private Message message;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Message {
+        private String role;
+        private String content;
+    }
+}
