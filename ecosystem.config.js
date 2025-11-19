cf64cf4bd4ecfff5ce766d4e1ce1ae049f63d7df
Marco <mass7813@gmail.com>
2025-11-19 15:01:05 -0300

    deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'CriaitorBackend',
+      script: 'java',
+      exec_mode: 'fork',
+
+      args: ['-jar', 'target/gerador-ideias-backend-0.0.1-SNAPSHOT.jar', '-Dspring.profiles.active=prod'],
+
+      cwd: 'E:/Criaitor/projeto-gerador-ideias-backend',
+      out_file: './logs/stdout.log',
+      error_file: './logs/stderr.log',
+    },
+  ],
+};
